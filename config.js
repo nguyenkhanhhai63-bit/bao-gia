ac551d7a82b43b3aca04e90eae2139b466c01ca3
nguyenkhanhhai63-bit <nguyenkhanhhai63@gmail.com>
2026-08-18 09:33:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Dùng đúng link TSV mà Google Sheet của bạn đã xuất bản
-const SHEET_DATA_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQydsSDfqCK17X4J_U7JQOsjIkt2brKfkv-wjpqTMVfvK20jGdbaE8cXnzzYi1latiwvA5rErbbPHFQ/pub?gid=704415736&single=true&output=tsv";
+const SHEET_DATA_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQydsSDfqCK17X4J_U7JQOsjIkt2brKfkv-wjpqTMVfvK20jGdbaE8cXnzzYi1latiwvA5rErbbPHFQ/pub?gid=704415736&single=true&output=csv";
 const AUTO_REFRESH_MS = 60000;
